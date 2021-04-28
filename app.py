6c3ecf00d11bcc0528461a269fc74e5cf49bdcf4
Ziiv <78029655+Ziiv-git@users.noreply.github.com>
2021-04-28 19:05:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    features = [x for x in request.form.values()]
+    features = [int(x) for x in request.form.values()]
     final_features = [np.array(features)]
     prediction = model.predict(final_features)
 
